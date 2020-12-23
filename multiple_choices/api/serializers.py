ede524d4b855bdd23aa92eddb5531b50afae5f18
mbijou <mimounbijjou@Mimouns-Air.fritz.box>
2020-12-23 21:07:55 +0100

    added solution serializer for multiple choice

--- multiple_choices/api/serializers.py
+++ multiple_choices/api/serializers.py
@@ -1,8 +1,17 @@
-from rest_framework.serializers import ModelSerializer
-from multiple_choices.models import MultipleChoice
+from rest_framework import serializers
+from multiple_choices.models import MultipleChoice, Solution
 
 
-class MultipleChoiceSerializer(ModelSerializer):
+class SolutionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Solution
+        fields = ('id', "answer", "solution", )
+
+
+class MultipleChoiceSerializer(serializers.ModelSerializer):
+    solution_set = SolutionSerializer(many=True)
+
     class Meta:
         model = MultipleChoice
-        fields = ("id", "question", "workload", "deadline",)
+        fields = ("id", "question", "workload", "deadline", "solution_set",)
+
