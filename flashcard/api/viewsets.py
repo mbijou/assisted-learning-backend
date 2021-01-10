3dd7ad05a68fc10c99d18e4cf8ff27f5c4688104
Totorok42 <57009029+Totorok42@users.noreply.github.com>
2021-01-11 00:02:00 +0100

    Changed Flashcard order in FlashcardViewSet

--- flashcard/api/viewsets.py
+++ flashcard/api/viewsets.py
@@ -7,12 +7,11 @@ from rest_framework.response import Response
 
 class FlashcardViewSet(ReadOnlyModelViewSet):
     serializer_class = FlashcardSerializer
-    queryset = Flashcard.objects.all()
+    queryset = Flashcard.objects.all().order_by("-id")
 
     @action(detail=False, url_path="rank-one-flashcards")
     def rank_one_flashcards(self, request, user_id=None):
         rank_one_flashcards = Flashcard.objects.filter(rank=1)[:1]
-        print("ASKOD")
         page = self.paginate_queryset(rank_one_flashcards)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
