d7b746a1967eabd73b260d775ca5a5ff8abf4e38
Mücahit Sagiroglu <k42bro@hotmail.de>
2021-02-02 01:05:20 +0100

    Fixed flashcard for user only

--- flashcard/api/viewsets.py
+++ flashcard/api/viewsets.py
@@ -1,4 +1,4 @@
-from django.db.models import F, Case, When, CharField, Value, Min, IntegerField
+from django.db.models import F, Case, When, CharField, Value, Min, IntegerField, Q
 from rest_framework.decorators import action
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from flashcard.api.serializers import FlashcardSerializer
@@ -28,10 +28,15 @@ class FlashcardViewSet(ReadOnlyModelViewSet):
                 default=Value('FAILED'),
                 output_field=CharField(),
             )
-        ).filter(status="OPEN")
+        ).filter(status='OPEN', user_id=user_id)
 
         lowest_rank = queryset.aggregate(lowest_rank=Min("rank")).get("lowest_rank")
-        rank_one_flashcards = Flashcard.objects.filter(rank=lowest_rank, user_id=user_id)[:1]
+        rank_one_flashcards = Flashcard.objects.filter(
+            rank=lowest_rank, user_id=user_id
+        )[:1]
 
         serializer = self.get_serializer(rank_one_flashcards, many=True)
         return Response(serializer.data)
+
+    def get_queryset(self):
+        return self.queryset.filter(user_id=self.kwargs.get("user_id"))
